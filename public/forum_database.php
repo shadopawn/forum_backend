d8bfd5f3fbae091057bdef2df2770c526bf4c815
Daniel Litt <daniel_litt@comcast.net>
2020-10-05 15:14:59 -0400

    Add test json response

--- public/forum_database.php
+++ public/forum_database.php
@@ -1,12 +1,25 @@
 <?php
 $servername = "localhost";
 $username = "root";
+$dbname = "forum";
 
 // Create connection
-$conn = new mysqli($servername, $username);
+$conn = new mysqli($servername, $username, "", $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "SELECT id, name FROM topic";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "\n";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
